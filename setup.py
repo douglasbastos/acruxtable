94c0f944a6bddd897e346b19022eefb8064acad8
Douglas Bastos <douglas.bastos@corp.globo.com>
2015-08-31 18:44:27 -0300

    Iniciando projeto

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='acruxtable',
+    description='Desafio sieve',
+    author='Douglas Bastos',
+    author_email='douglashsb@gmail.com',
+    url='https://bitbucket.org/douglasbastos/acruxtable',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
