688269fc22cb4986ab27e4e19c9ede6a80035696
Douglas Bastos <douglas.bastos@corp.globo.com>
2015-09-02 16:42:55 -0300

    Adiciona .json para a roda indicada

--- acruxtable/urls.py
+++ acruxtable/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import list_emails
 
 urlpatterns = [
-    url(r'list_emails', list_emails, name='list_emails'),
+    url(r'emails.json', list_emails, name='emails'),
 ]
