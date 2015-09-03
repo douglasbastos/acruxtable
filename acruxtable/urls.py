5ecacd92cd39c1f62ca9557b4f02164db37472d8
Douglas Bastos <douglas.bastos@corp.globo.com>
2015-09-03 18:32:12 -0300

    Cria rota para tabela de emails

--- acruxtable/urls.py
+++ acruxtable/urls.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from django.conf.urls import url
-from .views import list_emails
+from .views import list_emails, TableEmails
 
 urlpatterns = [
     url(r'emails.json', list_emails, name='emails'),
+    url(r'tabela-emails.html', TableEmails.as_view(), name='table_emails'),
 ]
