567c8131eb45efe40d7211b8a8a930baaba68e2b
Douglas Bastos <douglas.bastos@corp.globo.com>
2015-09-03 21:19:20 -0300

    Formata data

--- acruxtable/static/acruxtable/js/make_table.js
+++ acruxtable/static/acruxtable/js/make_table.js
@@ -38,11 +38,12 @@ function MakeTable(config) {
 
         createTable: function(items){
             table = '';
+            var self = this;
             $.each(items, function(i, item) {
                 table += [
                 '<tr>',
                     '<td>' + item.name + '</td>',
-                    '<td>' + item.date + '</td>',
+                    '<td>' + self.formatDate(item.date) + '</td>',
                     '<td>' + item.subject + '</td>',
                 '</tr>',
                 ].join('');
@@ -50,6 +51,28 @@ function MakeTable(config) {
             $('#conteudo').html(table)
         },
 
+        formatDate: function(date){
+            d = new Date(date);
+
+            // Chama método isToday
+            // return o resultado do método que será criado is_today
+            day = d.getDate();
+            month = d.getMonth()+1;
+            year = d.getFullYear();
+
+            console.log(day.toString().length);
+            if (day.toString().length === 1)
+                day = '0'+day;
+            if (month.toString().length === 1)
+                month = '0'+month;
+
+            return day +'/'+ month +'/'+ year;
+
+        }
+
+        isToday: function(date){
+            return false;
+        }
     };
 
 }(jQuery));
