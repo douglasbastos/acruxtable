bb96c99a807fe572d9c3e0a7c408d4f01254ca85
Douglas Bastos <douglas.bastos@corp.globo.com>
2015-09-09 16:58:07 -0300

    muda url do projeto e adiciona dependências

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='acruxtable',
-    description='Desafio sieve',
+    description='Desafio',
     author='Douglas Bastos',
     author_email='douglashsb@gmail.com',
-    url='https://bitbucket.org/douglasbastos/acruxtable',
+    url='https://github.com/douglasbastos/acruxtable',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[]
+    install_requires=[
+        'Django >= 1.7
+    ]
 )
